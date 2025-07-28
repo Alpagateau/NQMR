f669ae2e8fe77ed2a2ef2650ecab9952d2243c32
Alpa <alpaga.ytb@gmail.com>
2025-07-28 23:24:01 +0200

    Textures ARE WORKINGGG

--- build/NQMT/gfx/arrows.h
+++ build/NQMT/gfx/arrows.h
@@ -8,7 +8,7 @@
 //	+ 80 tiles not compressed
 //	Total size: 512 + 5120 = 5632
 //
-//	Time-stamp: 2025-07-28, 16:54:38
+//	Time-stamp: 2025-07-28, 23:22:03
 //	Exported by Cearn's GBA Image Transmogrifier, BlocksDS v1.10.0-blocks
 //	( http://www.coranac.com/projects/#grit )
 //
